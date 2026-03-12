e784f1f7787c06dddf8e0d849ccfdb1b7a499e14
Shrouk-Holmes <shrouk26529@gmail.com>
2026-03-13 00:20:51 +0200

    phase2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
-// app.use("/api/projects", projectRoutes);
+app.use("/api/projects", projectRoutes);
 
 app.use(globalErrorHandler);
 
